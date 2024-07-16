fcddb25fea99fc0e709054ef27842bd232c906a4
Renegade <sfoqus@gmail.com>
2024-07-16 15:50:29 +0400

    First and last commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+import React from 'react';
+
+import {QuizCard} from './components/QuizCard/QuizCard.js';
+import {QuizResult} from './components/QuizResult/QuizResult.js';
+
 import './App.css';
 
 function App() {
+  const questions = [{question: 'Что такое React?', ans1: 'Библеотека', ans2: 'Хз', ans3: '51 элемент таблицы Менделеева', correct: 1}, {question: 'Почему птицы любят горшки?', ans1: 'Илон Маск', ans2: 'Из за обильного количества любви', ans3: 'Не придумал короче', correct: 3}, {question: 'Почему корейка не любит корейскую кухню?', ans1: 'Корейка любит покакать', ans2: 'Я не хочу отвечать на этот вопрос', ans3: 'Корейка любит меня', correct: 2}]
+  const [count, setCount] = React.useState(0);
+  const [countCorrect, setCountCorrect] = React.useState(0);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      {count !== questions.length ? <QuizCard questions={questions} count={count} setCount={setCount} countCorrect={countCorrect} setCountCorrect={setCountCorrect}/> : <QuizResult setCount={setCount} amnt={questions.length} countCorrect={countCorrect} setCountCorrect={setCountCorrect}/>}
+
     </div>
   );
 }
